527e57afd564c019b2f63ef4714abef3d625b063
Sublicuentas <marvinvalenzuela293@gmail.com>
2026-07-24 16:16:55 -0600

    Update Firebase configuration with new credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
 window.PADONDE_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBkHep4Efy7z_wOZM-D5oqU8lDW-R4rMpU",
+  authDomain: "pa-donde-hn.firebaseapp.com",
+  projectId: "pa-donde-hn",
+  storageBucket: "pa-donde-hn.firebasestorage.app",
+  messagingSenderId: "232908314083",
+  appId: "1:232908314083:web:9ef19ace7eb162f002fcd6"
 };
-window.PADONDE_ADMIN_EMAIL = "su-correo@ejemplo.com";
+
+window.PADONDE_ADMIN_EMAIL = "padondehn@sublicuentas.com";
